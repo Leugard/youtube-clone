d205f23bb3c8f84397bcbe4f5729b27aee33b96f
Leugard <kirakazama01@gmail.com>
2025-04-20 19:05:32 +0700

    feat: integrate TRPC with Upstash Redis and implement rate limiting

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div className="">page</div>;
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Leugard" });
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
